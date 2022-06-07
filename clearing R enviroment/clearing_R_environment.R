928acf54cae36d198020e147ae163f1c10c570c9
Amanyiraho <amanyiraho@gmail.com>
2022-06-07 20:54:17 +0300

    updated the how to clean your environment

---
+++ clearing R enviroment/clearing_R_environment.R
@@ -0,0 +1,14 @@
+## Creating variables in the environment
+
+company <- "pearl data firm"
+
+country <-  "Uganda"
+
+products <-  "Mage Health"
+
+x <-  2
+
+
+## code to clear your R environment
+
+rm(list = ls())
